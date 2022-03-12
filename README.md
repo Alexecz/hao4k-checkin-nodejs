7ed937b74a204fd657118e2ca1b16cf2c1bee774
Bin Jiang <tfs5503@autuni.ac.nz>
2022-03-12 20:47:11 +0800

    改进原作者签到

---
+++ README.md
@@ -0,0 +1,41 @@
+改进原作者的签到 
+ ·增加动态获取formhash （每个人的formhash不同）
+ ·优化判断cookie是否生效
+原作者地址：https://github.com/ming917/hao4K-checkin
+
+
+### 建议 cookie 用该浏览器插件直接复制   直接点copy all
+https://chrome.google.com/webstore/detail/header-cookie-qrcode/echlhpliefhchnkmiomfpdnehakfmpfl/related?hl=zh-CN
+
+### 高能预警~
+
+hao4K自动签到来啦！！！
+
+#### 脚本功能：
+
+1、通过Github Action自动定时运行 app.js 脚本。
+
+2、通过cookies自动登录（[https://www.hao4k.cn/](https://www.hao4k.cn/))，脚本会自动进行checkin。
+
+3、然后通过“Server酱”（[http://sc.ftqq.com/3.version](http://sc.ftqq.com/3.version))，自动发通知到微信上。
+
+
+
+#### 食用姿势：
+
+1. 先“Fork”本仓库。（不需要修改任何文件！）
+
+2. 注册hao4K
+
+3. 登录hao4K后获取cookies。（简单获取方法：浏览器快捷键F12，打开调试窗口，点击“network”随便一个接口的请求头中获取）。cookie示例需要的关键信息：HxHg_2132_saltkey=xxx;HxHg_2132_auth=xxx;
+
+4. 在自己的仓库“Settings”里创建3个“Secrets => Actions => New repository secret”，分别是：（不开启通知，只需要创建一个COOKIE即可）
+
+   - COOKIE（**必填**）
+   - SERVE（server酱开关，默认是off，填on的话，会同时开启cookie失效通知和签到成功通知）
+   - SCKEY（填写server酱sckey，不开启server酱则不用填）
+
+5. 以上设置完毕后，每天零点会自动触发，并会执行自动checkin，如果开启server酱，会自动发通知到微信上。
+
+6. **如果以上都不会的话，注册hao4K后，每天勤奋点记得登录后手动进行checkin即可。**
+
---
+++ app.js
@@ -0,0 +1,103 @@
+import axios from "axios";
+import xmlJs from "xml-js";
+import iconv from "iconv-lite";
+import * as cheerio from 'cheerio';
+
+// server酱开关，填off不开启(默认)，填on同时开启cookie失效通知和签到成功通知
+const server = process.env["SERVER"];
+
+// 填写server酱sckey,不开启server酱则不用填
+const sckey = process.env["SCKEY"];
+
+// 填入glados账号对应cookie
+const cookie = process.env["COOKIE"];
+
+const loginUrl =
+  "https://www.hao4k.cn/qiandao/";
+
+const userAgent =
+  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36 Edg/99.0.1150.39";
+
+const headers = {
+  cookie: cookie ?? "",
+  "User-Agent": userAgent,
+  "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
+};
+
+function getFormHash() {
+  axios
+    .get(loginUrl, {
+      headers,
+      responseType: "arraybuffer",
+    })
+    .then((response) => {
+      const gb = iconv.decode(response.data, "gb2312");
+      const $ = cheerio.load(gb);
+      let formHash = '';
+      // console.log($.html())
+      const userName = $('#mumucms_username').text();
+      if (userName == '') {
+        console.log("cookie失效！");
+        let message = "cookie失效！";
+        pushNotice(message);
+      } else {
+        console.log("获取用户信息成功！")
+        formHash = $('#scbar_form input').eq(1).val();
+        checkin(formHash);
+      }
+    })
+    .catch((error) => {
+      console.log("hao4K:获取formhash出错" + error);
+    });
+}
+
+function checkin(formHash) {
+  const checkInUrl =
+    "https://www.hao4k.cn//qiandao/?mod=sign&operation=qiandao&formhash="+ formHash +"&format=empty&inajax=1&ajaxtarget=";
+  axios
+    .get(checkInUrl, {
+      headers,
+      responseType: "arraybuffer",
+    })
+    .then((response) => {
+      const resUtf8 = iconv.decode(response.data, "GBK");
+      const dataStr = xmlJs.xml2json(resUtf8, {
+        compact: true,
+        spaces: 4,
+      });
+      const data = JSON.parse(dataStr);
+      console.log(data);
+      const content = data?.root?._cdata;
+      let message = "";
+      if (content) {
+        if (content === "今日已签") {
+          message = "hao4K:今日已签";
+          console.log(message);
+        }
+      } else {
+        message = "hao4K:签到成功!";
+        console.log(message);
+      }
+
+      // 解决 Request path contains unescaped characters
+      message = encodeURI(message);
+      pushNotice(message)
+    })
+    .catch((error) => {
+      console.log("hao4K:签到出错" + error);
+    });
+}
+
+function pushNotice(message) {
+  if (server === "on") {
+    axios
+      .get("https://sc.ftqq.com/" + sckey + ".send?text=" + message)
+      // 解决 UnhandledPromiseRejectionWarning
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+}
+
+
+getFormHash();
